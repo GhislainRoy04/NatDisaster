f03979b1a7ac803d7cd4528878bff8776e979156
Ghislain Roy <ghislainroy.04@gmail.com>
2017-07-18 15:12:58 -0300

    Show basic info. The style will be modified later. Just adding basic stuff

--- src/disaster/screen/ColdWaveScreen.js
+++ src/disaster/screen/ColdWaveScreen.js
@@ -1,6 +1,8 @@
 'use strict';
 import React,{Component} from "react";
-import {View,Text} from "react-native";
+import {Text,View,Linking} from "react-native";
+import {Button} from "react-native-elements";
+import {Api} from "../../api";
 
 class ColdWaveScreen extends Component{
     static navigationOptions = ({navigation}) =>({
@@ -9,14 +11,39 @@ class ColdWaveScreen extends Component{
 
     constructor(props){
         super(props);
+        this.state=({pastEvent:[],currentEvent:[],alert:[],reports:[]});
+        this.onReportPress=this.onReportPress.bind(this);
+        this.moreInfo = this.onReportPress.bind(this);
     }
 
     render(){
         return(
             <View>
                 <Text>Cold wave</Text>
+                <Button title="Reports" onPress={()=>{this.onReportPress()}} />
+
+                {this.state.reports.map((report,index)=>
+
+                <View key={index}>
+                    <Text>{report.fields.title}</Text>
+                    <Button title="More info" onPress={() => {
+                        Linking.openURL(report.href).catch('Error occurred trying to open link.');
+                    }}/>
+                </View>
+
+                )}
             </View>
         )
+    }
+
+    onReportPress(){
+        Api.getDisasterByType('coldWave').then((res) => {
+            this.setState({reports:res.data.data});
+        });
+    }
+
+    moreInfo(uri){
+        this.setState({selectedUri:uri});
     }
 }
 
