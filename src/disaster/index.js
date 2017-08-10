1ec3424f6106e79647032b2b719acbecd55fd688
Ghislain Roy <ghislainroy.04@gmail.com>
2017-08-10 14:02:16 -0300

    Seperated the disaster navigation into it's own component

--- src/disaster/index.js
+++ src/disaster/index.js
@@ -1,3 +1,4 @@
 'use strict';
 export {default as DisasterScreen} from "./screen/DisasterScreen";
 export {default as MainDisasterScreen} from "./screen/MainDisasterScreen";
+export {default as Stack} from "./navigation/DisasterNavigation";
--- src/navigation/screen/Navigation.js
+++ src/navigation/screen/Navigation.js
@@ -1,11 +1,11 @@
 'use strict';
 import React, {Component} from "react";
 import {StackNavigator, DrawerNavigator, DrawerItems} from "react-navigation";
-import {ScrollView,AsyncStorage,Platform} from "react-native";
+import {ScrollView,AsyncStorage} from "react-native";
 import {DashboardScreen} from "../../dashboard";
 import {LoginScreen} from "../../login";
 import {Settings} from "../../settings";
-import {DisasterScreen, MainDisasterScreen} from "../../disaster";
+import {Stack} from "../../disaster";
 
 class Navigation extends Component {
     constructor(props){
@@ -20,159 +20,6 @@ class Navigation extends Component {
     }
     render() {
         let {token}= this.state;
-
-        const Stack = {
-                default: {
-                    screen: MainDisasterScreen,
-                    headerMode: "float",
-                    navigationOptions: {
-                        headerStyle: {
-                            marginTop: Platform.OS==="android"?25:0
-                        }
-                    }
-                },
-                earthquake: {
-                    screen: DisasterScreen,
-                    headerMode: "float",
-                    navigationOptions: {
-                        title: "Earthquake",
-                        headerStyle: {
-                            marginTop: Platform.OS==="android"?25:0
-                        }
-                    }
-                },
-                volcano: {
-                    screen: DisasterScreen,
-                    navigationOptions: {
-                        title: "Volcano",
-                        headerStyle: {
-                            marginTop: Platform.OS==="android"?25:0
-                        }
-                    }
-                },
-                avalanche: {
-                    screen: DisasterScreen, headerMode: "float",
-                    navigationOptions: {
-                        title: "Avalanche",
-                        headerStyle: {
-                            marginTop: Platform.OS==="android"?25:0
-                        }
-                    }
-                },
-                coldWave: {
-                    screen: DisasterScreen, headerMode: "float",
-                    navigationOptions: {
-                        title: "Cold Wave",
-                        headerStyle: {
-                            marginTop: Platform.OS==="android"?25:0
-                        }
-                    }
-                },
-                cyclone: {
-                    screen: DisasterScreen, headerMode: "float",
-                    navigationOptions: {
-                        title: "Cyclone",
-                        headerStyle: {
-                            marginTop: Platform.OS==="android"?25:0
-                        }
-                    }
-                },
-                drought: {
-                    screen: DisasterScreen, headerMode: "float",
-                    navigationOptions: {
-                        title: "Drought",
-                        headerStyle: {
-                            marginTop: Platform.OS==="android"?25:0
-                        }
-                    }
-                },
-                epidemic: {
-                    screen: DisasterScreen, headerMode: "float",
-                    navigationOptions: {
-                        title: "Viral Outbreak",
-                        headerStyle: {
-
-                            marginTop: Platform.OS==="android"?25:0
-                        }
-                    }
-                },
-                fire: {
-                    screen: DisasterScreen, headerMode: "float",
-                    navigationOptions: {
-                        title: "Fire / Wild Fire",
-                        headerStyle: {
-                            marginTop: Platform.OS==="android"?25:0
-                        }
-                    }
-                },
-                flashFlood: {
-                    screen: DisasterScreen, headerMode: "float",
-                    navigationOptions: {
-                        title: "Flash Flood",
-                        headerStyle: {
-                            marginTop: Platform.OS==="android"?25:0
-                        }
-                    }
-                },
-                flood: {
-                    screen: DisasterScreen, headerMode: "float",
-                    navigationOptions: {
-                        title: "Flood",
-                        headerStyle: {
-                            marginTop: Platform.OS==="android"?25:0
-                        }
-                    }
-                },
-                heatWave: {
-                    screen: DisasterScreen, headerMode: "float",
-                    navigationOptions: {
-                        title: "Heat Wave",
-                        headerStyle: {
-                            marginTop: 25
-                        }
-                    }
-                },
-                landMud: {
-                    screen: DisasterScreen, headerMode: "float",
-                    navigationOptions: {
-                        title: "Land Slide",
-                        headerStyle: {
-                            marginTop: 25
-                        }
-                    }
-                },
-                localStorm: {
-                    screen: DisasterScreen, headerMode: "float",
-                    navigationOptions: {
-                        title: "Local Storm",
-                        headerStyle: {
-                            marginTop: 25
-                        }
-                    }
-                },
-                stormSurge: {
-                    screen: DisasterScreen, headerMode: "float",
-                    navigationOptions: {
-                        title: "Storm Surge",
-                        headerStyle: {
-                            marginTop: 25
-                        }
-                    }
-                },
-                tsunami: {
-                    screen: DisasterScreen, headerMode: "float",
-                    navigationOptions: {
-                        title: "Tsunami",
-                        headerStyle: {
-                            marginTop: 25
-                        }
-                    }
-                },
-            login: {
-                screen: LoginScreen,
-            },
-            };
-
         const DrawerRoutes = DrawerNavigator({
             dashboard: {
                 name: 'Dashboard',
